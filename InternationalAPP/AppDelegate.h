dce8178082ba1a9f41413fbd5630ff18f0c8c377
mingjin <727540797@qq.com>
2017-01-24 09:46:25 +0800

    Initial Commit

---
+++ InternationalAPP/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  InternationalAPP
+//
+//  Created by none on 17/1/24.
+//  Copyright © 2017年 mj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
